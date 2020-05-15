4b367d616bed1ce0b6b40faf2ce0504419fd1dd9
Khaled Elshamy <khaled.elshamy79@yahoo.com>
2020-05-15 02:23:47 +0200

    Create 334. Increasing Triplet Subsequence.cpp

---
+++ LeetCode/334. Increasing Triplet Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        if(nums.size()<3)return false;
+        
+        int firstMin = INT_MAX;
+        int secondMin = INT_MAX;
+        
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<= firstMin)firstMin = nums[i];
+            else if(nums[i]<= secondMin)secondMin = nums[i];
+            else return true ;
+        }
+        
+        return false ;
+    }
+};
